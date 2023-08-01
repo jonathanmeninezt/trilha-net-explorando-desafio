f22f8bd86f94fc4cf0fcb766623f022e0b6e9b54
Jonathan Meninez <116603241+jonathanmeninezt@users.noreply.github.com>
2023-08-01 19:18:19 -0300

    criação - Reserva.cs - ObterQuantidadeHospedes / CalcularValorDiaria

--- Program.cs
+++ Program.cs
@@ -6,20 +6,22 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa(nome: "Antônio Pereira da Silva");
+Pessoa p2 = new Pessoa(nome: "Julia Silva da Ferreira");
+Pessoa p3 = new Pessoa(nome: "Pedro Pereira Ferreira");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 130);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine($"Valor total: {reserva.CalcularValorDiaria()}");
